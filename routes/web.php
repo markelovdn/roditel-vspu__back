e9987d41680f4d5e0a5cc0f8a9d461d9ff92d32a
Дмитрий <markelovdn@gmail.com>
2023-09-01 13:32:06 +0300

    close web route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/', function () {
+//     return 'Somthing went wrong';
+// });
+
+Route::get('/{any}', function () {
+    return 'Somthing went wrong';
+})->where('any','.*');
+
