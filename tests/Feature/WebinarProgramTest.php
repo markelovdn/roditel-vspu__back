dd6d76a94510ff59db3d585b6e321ac73cbae7b9
Дмитрий <markelovdn@gmail.com>
2023-09-19 11:16:08 +0300

    fix webinar name

--- tests/Feature/WebinarProgramTest.php
+++ tests/Feature/WebinarProgramTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class VabinarProgramTest extends TestCase
+class WebinarProgramTest extends TestCase
 {
     /**
      * A basic feature test example.
