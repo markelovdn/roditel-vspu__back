32009b717ac92de8ed935ea1f874fbec985014e2
Дмитрий <markelovdn@gmail.com>
2023-11-14 09:41:34 +0300

    consultation messages test

--- tests/Feature/ConsultationMessagesTest.php
+++ tests/Feature/ConsultationMessagesTest.php
@@ -12,12 +12,11 @@ use Tests\TestCase;
 
 class ConsultationMessagesTest extends TestCase
 {
-    public function testStore()
+    public function test_store()
     {
-
-        $user = User::where('id', 93)->first();
+        $consultation = Consultation::first();
+        $user = User::where('id', $consultation->user_id)->first();
         Auth::login($user);
-        $consultation = Consultation::where('user_id', $user->id)->first();
 
         $response = $this->post('/api/consultations/'.$consultation->id.'/messages', [
             'text' => 'Тестовый текст',
@@ -28,6 +27,54 @@ class ConsultationMessagesTest extends TestCase
 
         $response->assertStatus(200)
         ->assertJsonFragment(['message' => 'Message successfully added']);
+    }
 
+    public function test_show(): void
+    {
+        $consultation = Consultation::first();
+        $message = $consultation->messages()->first();
+        $user = User::where('id', $consultation->user_id)->first();
+        Auth::login($user);
+
+        $response = $this->get('/api/messages/'.$message->id);
+        // $response->dd();
+
+        $response->assertStatus(200)
+        ->assertJsonFragment(['userId' => $user->id]);
+    }
+
+    public function test_update()
+    {
+        $consultation = Consultation::first();
+        $message = $consultation->messages()->first();
+        $user = User::where('id', $consultation->user_id)->first();
+        Auth::login($user);
+
+        $response = $this->put('/api/messages/'.$message->id, [
+            'text' => 'Тестовый текст обновленный',
+            'consultationId' => $consultation->id,
+            'user_id' => $user->id,
+            'readed' => false,
+        ]);
+
+        $response->assertStatus(200)
+        ->assertJsonFragment(['message' => 'Message successfully updated']);
+    }
+
+    public function test_destroy(): void
+    {
+        $consultation = Consultation::first();
+        $message = $consultation->messages()->first();
+        $user = User::where('id', $consultation->user_id)->first();
+        Auth::login($user);
+
+        $response = $this->delete('/api/messages/'.$message->id);
+
+        $this->assertDatabaseMissing('consultation_messages', [
+            'id' => $message->id
+        ]);
+
+        $response->assertStatus(200)
+        ->assertJsonIsObject();
     }
 }
